13d9a96ac810eb45677cc610a606d797b0cbb001
Samir Cabrera <samircm1406@gmail.com>
2020-02-17 20:33:15 +0100

    creacion de home component, implement angular material

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { HomeComponent } from './home.component';
+import { AngularMaterialModule } from '../../angular-material/angular-material.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HomeComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    AngularMaterialModule
   ]
 })
 export class HomeModule { }
