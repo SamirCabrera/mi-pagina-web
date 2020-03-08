ee2802d83c373700357706842b807a388a14c818
Samir Cabrera <samircm1406@gmail.com>
2020-03-08 05:02:11 +0100

    update

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,9 +7,78 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public list: Array<any>;
+  public matGrid: Array<any>;
+  public titleImgCabecera: Array<any>;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.listNav();
+    this.infoCenter();
+    this.titleImg();
+  }
+
+  private listNav() {
+    this.list = [
+      {
+        name: 'Categorias'
+      },
+      {
+        name: 'Novedades'
+      },
+      {
+        name: 'Ofertas'
+      },
+      {
+        name: 'Hombre'
+      },
+      {
+        name: 'Mujeres'
+      },
+    ]
+  }
+
+  private infoCenter() {
+    this.matGrid = [
+      {
+        cols: 6,
+        rows: 2,
+        title: true
+      },
+      {
+        cols: 6,
+        rows: 4,
+        img: true
+      },
+      {
+        cols: 6,
+        rows: 1,
+        description: true
+      },
+      {
+        cols: 6,
+        rows: 1,
+        boton: true
+      }
+    ]
+  }
+
+  private titleImg() {
+    this.titleImgCabecera = [
+      {
+        title: 'Messi 97'
+      },
+      {
+        title: 'CR7 201'
+      },
+      {
+        title: 'LeBron Ax'
+      },
+      {
+        title: 'Curry25'
+      },
+    ]
   }
 
 }
