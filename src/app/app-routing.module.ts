a35d07ec72e48aa1231d6a9fa03ad63a8da83f52
michael130199 <michael130199@gmail.com>
2020-03-11 17:18:16 -0400

    agregando el modulo shopping-cart

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { HomepageComponent } from './pages/homepage/homepage.component';
+import { ShoppingCartComponent } from './pages/shopping-cart/shopping-cart.component';
 
 
 const routes: Routes = [ 
   { path: '', redirectTo: 'homepage', pathMatch: 'full' },
   { path: 'homepage', component: HomepageComponent },
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'shopping-cart', component: ShoppingCartComponent}
 ];
 
 @NgModule({
